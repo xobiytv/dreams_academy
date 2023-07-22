99c839718419972689836a161a189cf9324000ad
Diyor <xobiytv.gmail.com>
2023-07-22 16:23:23 +0500

    struktura

---
+++ js/app.js
@@ -0,0 +1,21 @@
+{
+  /* <script type="module"> */
+}
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs";
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
